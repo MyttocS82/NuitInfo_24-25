3ffb72ee8b436bbee08b16a3b1d093afa5b240cf
Scott Michelon <scott.michelon@etu.iut-tlse3.fr>
2024-10-29 11:52:33 +0100

    2e commit - pré nuit de l'info

--- defi1/src/App.js
+++ defi1/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header() {
+    return (
+        <div className="App-header">
+            <a href={'/App.js'}><img src={logo} className="App-logo" alt="logo" /></a>
+            <nav className="App-nav">
+                <ul>
+                    <a href={'../public/index.html'} ><li className="App-nav-item">Home</li></a>
+                    <a href={'./App.test.js'}><li className="App-nav-item">Nuit de l'Info</li></a>
+                    <a href={'./Contact.js'}><li className="App-nav-item">Contact</li></a>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </header>*/}
+            <Header />
+        </div>
+    );
 }
 
 export default App;
